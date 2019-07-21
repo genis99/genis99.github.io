dedab690d3ef05ebe3ccbc868cecf9e3b850f9ed
genis99 <40755369+genis99@users.noreply.github.com>
2019-07-22 01:40:19 +0200

    c

---
+++ web.js
@@ -0,0 +1,57 @@
+var rvisible = true;
+var bvisible = false;
+var gvisible = false;
+var myVar = setInterval(myTimer, 4000);
+function myTimer() {
+    if (rvisible) {
+        document.getElementById("red").style.opacity = '0';
+        document.getElementById("blue").style.opacity = '1';
+        bvisible = true;
+        rvisible = false;
+    }
+    else if (bvisible) {
+        document.getElementById("blue").style.opacity = '0';
+        document.getElementById("green").style.opacity = '1';
+        gvisible = true;
+        bvisible = false;
+    }
+    else if (gvisible) {
+        document.getElementById("green").style.opacity = '0';
+        document.getElementById("red").style.opacity = '1';
+        rvisible = true;
+        gvisible = false;
+    }
+}
+function myfunction1() {
+    document.getElementById("red").style.opacity = '1';
+    document.getElementById("blue").style.opacity = '0';
+    document.getElementById("green").style.opacity = '0';
+    rvisible = true;
+    bvisible = false;
+    visible = false;
+}
+function myfunction2() {
+    document.getElementById("red").style.opacity = '0';
+    document.getElementById("blue").style.opacity = '1';
+    document.getElementById("green").style.opacity = '0';
+    rvisible = false;
+    bvisible = true;
+    gvisible = false;        
+}
+function myfunction3() {
+    document.getElementById("red").style.opacity = '0';
+    document.getElementById("blue").style.opacity = '0';
+    document.getElementById("green").style.opacity = '1';
+    rvisible = false;
+    bvisible = false;
+    gvisible = true;        
+}
+function myFunction1() {
+    myfunction1(); clearTimeout(myVar); myVar = setInterval(myTimer, 4000);
+}
+function myFunction2() {
+    myfunction2(); clearTimeout(myVar); myVar = setInterval(myTimer, 4000); 
+}
+function myFunction3() {
+    myfunction3(); clearTimeout(myVar); myVar = setInterval(myTimer, 4000);
+}
